c7c812a093abaca0690b06388059b17e0343027c
Silva, Guilherme (G.M.) <monjogui@gmail.com>
2026-06-04 16:01:27 -0300

    update: kaggle setup updates

--- projeto_tabd/src/main/java/com/project/services/tasks/ApiConsumer.java
+++ projeto_tabd/src/main/java/com/project/services/tasks/ApiConsumer.java
@@ -1,6 +1,10 @@
 package com.project.services.tasks;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class ApiConsumer extends Thread {
+
+        Dotenv dotenv = Dotenv.configure().directory("projeto_tabd/assets").load();
 
     String dataset = "";
     private long response;
@@ -11,14 +15,37 @@ public class ApiConsumer extends Thread {
 
     public void fetchDataset() {
 
-        
+            ProcessBuilder pb1 = new ProcessBuilder(
+                "pip", "install", "kaggle"
+            );
+
+            ProcessBuilder pb2 = new ProcessBuilder(
+                "cmd.exe", "/c", "md", "%USERPROFILE%\\.kaggle"
+            );
+
+            ProcessBuilder pb3 = new ProcessBuilder(
+                 "cmd.exe", "/c", "echo {\"username\":\"" + dotenv.get("KAGGLE_USERNAME") + "\",\"key\":\"" + dotenv.get("KAGGLE_API_TOKEN") + "\"} > %USERPROFILE%\\.kaggle\\kaggle.json"
+            );
+
             ProcessBuilder pb = new ProcessBuilder(
-                    "kaggle", "datasets", "download", "-d", this.dataset, "--path",  "../../../../../../datasets",
+                    "kaggle", "datasets", "download", "-d", this.dataset, "--path",  "../../../../../../../assets/datasets",
                     "--unzip"
 
             );
 
             try {
+                pb1.inheritIO();
+                Process process1 = pb1.start();
+                process1.waitFor();
+
+                pb2.inheritIO();
+                Process process2 = pb2.start();
+                process2.waitFor();
+
+                pb3.inheritIO();
+                Process process3 = pb3.start();
+                process3.waitFor();
+
                 pb.inheritIO();
                 Process process = pb.start();
                 int exitCode = process.waitFor();
